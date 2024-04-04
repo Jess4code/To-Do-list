cf1f4151cbebcf2e848542720d52980072e530ed
Jess4code <jess4code@gmail.com>
2024-04-04 16:27:55 +0300

    used import

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,5 +1,4 @@
-let tasks = document.querySelector(".tasks");
-let list = document.createElement("ul");
+import { addTask, btnRemove, visibilityChecker, list, tasks } from "./vendor.js";
 let inputBtn = document.querySelector(".add");
 inputBtn.onclick = addTask;
 
@@ -18,38 +17,6 @@ if(localStorage.length !== 0) {
 }
 document.querySelector(".input").focus();
 }
-
-
-function addTask() {
-    let inputValue = document.querySelector(".input").value.trim();
-    if (inputValue !== ""){
-        let newEl = document.createElement("li");
-        let id = Date.now();
-        newEl.id = id;
-        newEl.innerHTML = `${inputValue} <button class="delete">Delete</button>`
-        newEl.querySelector(".delete").onclick = btnRemove;
-        list.appendChild(newEl);
-        localStorage.setItem(id, inputValue);
-    }
-    document.querySelector(".input").value = '';
-    tasks.appendChild(list);
-    visibilityChecker();
-    document.querySelector(".input").focus();
-};
-
-function btnRemove(el) {
-    el.currentTarget.parentElement.remove();
-    localStorage.removeItem(`${el.currentTarget.parentElement.id}`);
-    visibilityChecker();
-}
-
-function visibilityChecker () {
-let lis = document.querySelectorAll("li");
-if (lis.length === 0) {
-    tasks.classList.add("hidden");
-} else {
-    tasks.classList.remove("hidden");
-}};
 
 let scroller = document.querySelector(".scroller");
 
---
+++ Project/js/vendor.js
@@ -0,0 +1,35 @@
+let list = document.createElement("ul");
+let tasks = document.querySelector(".tasks");
+
+function addTask() {
+    let inputValue = document.querySelector(".input").value.trim();
+    if (inputValue !== ""){
+        let newEl = document.createElement("li");
+        let id = Date.now();
+        newEl.id = id;
+        newEl.innerHTML = `${inputValue} <button class="delete">Delete</button>`
+        newEl.querySelector(".delete").onclick = btnRemove;
+        list.appendChild(newEl);
+        localStorage.setItem(id, inputValue);
+    }
+    document.querySelector(".input").value = '';
+    tasks.appendChild(list);
+    visibilityChecker();
+    document.querySelector(".input").focus();
+};
+
+function btnRemove(el) {
+    el.currentTarget.parentElement.remove();
+    localStorage.removeItem(`${el.currentTarget.parentElement.id}`);
+    visibilityChecker();
+}
+
+function visibilityChecker () {
+let lis = document.querySelectorAll("li");
+if (lis.length === 0) {
+    tasks.classList.add("hidden");
+} else {
+    tasks.classList.remove("hidden");
+}};
+
+export {addTask, btnRemove, visibilityChecker, list, tasks}
